36632fa2ecb76e418eef82d3c6d39ced359901a2
Vladimir Kozlov <kozlov.vlr@yandex.ru>
2022-08-07 18:03:03 +0300

    condition for first plot changed - now first column excluded from print loop

--- plotter.py
+++ plotter.py
@@ -2,19 +2,18 @@ import matplotlib.pyplot as plt
 import os
 import pandas as pd
 
-main_plot_filename = 'log.csv'
-secondary_plot_filename = 'log_hysys.csv'
+main_plot_filename = 'log_solver_chlorine.csv'
+secondary_plot_filename = 'log_hysys_chlorine.csv'
 
 main_plot_data = pd.read_csv(main_plot_filename)
 secondary_plot_data = pd.read_csv(secondary_plot_filename, sep= ';')
-print(secondary_plot_data)
 
 fig, axes = plt.subplots(3, 1)
 axes[0].set_title('Composition, Temperature of Reaction Mixture and Reaction Rates vs. Length')
 axes[0].grid()
 axes[0].set_ylabel('Molar Fraction, [mol. frac.]')
-for param in main_plot_data.columns[0:-1]:
-	if 'rate' not in param and 'FLMASS' not in param and 'FLMOL' not in param and 'l' not in param:
+for param in main_plot_data.columns[1:-1]:
+	if 'rate' not in param and 'FLMASS' not in param and 'FLMOL' not in param:
 		axes[0].plot(main_plot_data['l'], main_plot_data[param], label=param, color= 'red', linewidth= 0.5)
 		axes[0].plot(secondary_plot_data['l'], secondary_plot_data[param], label=param,
 					 color= 'blue', linewidth= 0.5, linestyle= '--')
@@ -28,7 +27,6 @@ axes[1].plot(secondary_plot_data['l'], secondary_plot_data['T'], color= 'blue', 
 axes[2].set_ylabel('Reaction Rates, [kgmol/(m3*s)]')
 for param in main_plot_data.columns[0:-1]:
 	if 'rate' in param:
-		print(main_plot_data[param])
 		axes[2].plot(main_plot_data['l'], main_plot_data[param], label=param, color= 'red', linewidth= 0.5)
 		axes[2].plot(secondary_plot_data['l'], secondary_plot_data[param], label=param,
 					 color= 'blue', linewidth= 0.5, linestyle= '--')
