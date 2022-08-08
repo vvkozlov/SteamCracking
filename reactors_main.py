2ec55b2d666832fb213c5ab0165d70e20adcea16
Vladimir Kozlov <kozlov.vlr@yandex.ru>
2022-08-08 16:03:00 +0300

    input data moved out from 'reactors_main.py'

--- reactors_main.py
+++ reactors_main.py
@@ -17,45 +17,30 @@ import database_v2 as db
 import eos_engine as eos
 import subprocess as sp
 from eos_engine import flash_calc_PR_EOS_z_only as get_z_factor
+import reactors_config as cfg
 
 print('Initializing calculations...')
 
-'''Setting up reagents for both reactions'''
-PROPANE = db.PROPANE
-PROPYLENE = db.PROPYLENE
-ETHANE = db.ETHANE
-ETHYLENE = db.ETHYLENE
-H2 = db.H2
-
-'''Setting up reactions themselves'''
-rxn1 = db.rxn3
-rxn2 = db.rxn4
-rxnset1 = [rxn1, rxn2]
-
-'''Compsets for individual rxns'''
-rxn1_compset = rxn1.reagents
-rxn2_compset = rxn2.reagents
-'''Compset for all species in reactor'''
-rctr_compset = list(set(rxn1_compset + rxn2_compset))
-
-'''Feed stream mole composition [mol. frac.]'''
-comp_x0 = dict({ETHANE.name : 0.8, PROPANE.name : 0.2, ETHYLENE.name : 0, PROPYLENE.name : 0, H2.name: 0})
+'''Setting up reactions'''
+rxnset = cfg.rxnset
+rctr_compset = cfg.rctr_compset
+comp_x0 = cfg.comp_x0
 
 '''Setting up initial conditions'''
-molflow = 1  # Feed stream molar flow [kgmol/hr]
-P = 0.1  # Reaction Pressure [MPa]
-T0 = 1000 + 273.15  # Initial Temperature [K]
+molflow = cfg.molflow  # Feed stream molar flow [kgmol/hr]
+P = cfg.P  # Reaction Pressure [MPa]
+T0 = cfg.T0  # Initial Temperature [K]
 
 '''Reactor rating'''
-tube_L = 10000  # Reaction tubes length [mm]
-tube_ID = 50  # Reaction tubes Internal Diameter [mm]
-tubes_No = 1  # Reaction tubes number
+tube_L = cfg.tube_L  # Reaction tubes length [mm]
+tube_ID = cfg.tube_ID  # Reaction tubes Internal Diameter [mm]
+tubes_No = cfg.tubes_No  # Reaction tubes number
 
 '''Initializing feed Stream'''
 inlet_stream = rctr.Stream(rctr_compset, comp_x0, molflow, P, T0)
 
 '''Creating Reactor model'''
-cstreactor = rctr.PFRreactor(tube_L / 1000, tube_ID / 1000, tubes_No, rxnset1)
+cstreactor = rctr.PFRreactor(tube_L / 1000, tube_ID / 1000, tubes_No, rxnset)
 print('Starting calculations...')
 
 '''Integrating through PFReactor model'''
