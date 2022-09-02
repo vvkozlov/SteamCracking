e413435a95d6348ce49a4140a71ad75a7d5374aa
Vladimir Kozlov <kozlov.vlr@yandex.ru>
2022-09-02 13:14:34 +0300

    update input dataframes columns names

--- plotter.py
+++ plotter.py
@@ -11,8 +11,8 @@ import os
 import pandas as pd
 
 
-main_plot_filename = 'log_solver_chlorine.csv'
-secondary_plot_filename = 'log_hysys_chlorine.csv'
+main_plot_filename = 'log.csv'
+secondary_plot_filename = 'log_hysys_dante.csv'
 
 main_plot_data = pd.read_csv(main_plot_filename)
 secondary_plot_data = pd.read_csv(secondary_plot_filename, sep= ';')
@@ -22,22 +22,22 @@ axes[0].set_title('Composition, Temperature of Reaction Mixture and Reaction Rat
 axes[0].grid()
 axes[0].set_ylabel('Molar Fraction, [mol. frac.]')
 for param in main_plot_data.columns[1:-1]:
-	if 'rate' not in param and 'FLMASS' not in param and 'FLMOL' not in param and param != 't':
-		axes[0].plot(main_plot_data['l'], main_plot_data[param], label=param + '-solver', color= 'red', linewidth= 0.5)
-		axes[0].plot(secondary_plot_data['l'], secondary_plot_data[param], label=param + '-hysys',
+	if 'rate' not in param and 'FLMASS' not in param and 'FLMOL' not in param and param != 't [s]':
+		axes[0].plot(main_plot_data['l [m]'], main_plot_data[param], label=param + '-solver', color= 'red', linewidth= 0.5)
+		axes[0].plot(secondary_plot_data['l [m]'], secondary_plot_data[param], label=param + '-hysys',
 					 color= 'blue', linewidth= 0.5, linestyle= '--')
 
 axes[0].legend()
 axes[1].set_ylabel('Temperature, [K]')
 axes[1].set_xlabel('Length, [m]')
 axes[1].grid()
-axes[1].plot(main_plot_data['l'], main_plot_data['T'], color= 'red', linewidth= 0.5)
-axes[1].plot(secondary_plot_data['l'], secondary_plot_data['T'], color= 'blue', linewidth= 0.5, linestyle= '--')
+axes[1].plot(main_plot_data['l [m]'], main_plot_data['T [K]'], color= 'red', linewidth= 0.5)
+axes[1].plot(secondary_plot_data['l [m]'], secondary_plot_data['T [K]'], color= 'blue', linewidth= 0.5, linestyle= '--')
 axes[2].set_ylabel('Reaction Rates, [kgmol/(m3*s)]')
 for param in main_plot_data.columns[0:-1]:
 	if 'rate' in param:
-		axes[2].plot(main_plot_data['l'], main_plot_data[param], label=param + '-solver', color= 'red', linewidth= 0.5)
-		axes[2].plot(secondary_plot_data['l'], secondary_plot_data[param], label=param + '-hysys',
+		axes[2].plot(main_plot_data['l [m]'], main_plot_data[param], label=param + '-solver', color= 'red', linewidth= 0.5)
+		axes[2].plot(secondary_plot_data['l [m]'], secondary_plot_data[param], label=param + '-hysys',
 					 color= 'blue', linewidth= 0.5, linestyle= '--')
 axes[2].legend()
 
